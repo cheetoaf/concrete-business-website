84e2d63fdb42fd336e37e11be66d2d393f828207
cheetoaf <cheetoaf@gmail.com>
2024-06-27 00:41:35 -0700

    updated routing in App.js

--- src/App.js
+++ src/App.js
@@ -1,33 +1,36 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import logo from './assets/logo.svg';
 import Navbar from './components/Navbar.js';
 import About from './pages/About.js';
 import Contact from './pages/Contact.js';
 import Home from './pages/Home.js';
 import Services from './pages/Services.js';
-
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={About />} />
+        <Route path="/contact" element={Contact />} />
+        <Route path="/service" element={Services />} />
+
+
+      </Routes>
+
+
+
+
+    </Router>
+
+
+
+  )
+
+
 }
 
 export default App;
