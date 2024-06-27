d4df0a26998779c6fe7c38e548386a8a09b24893
cheetoaf <cheetoaf@gmail.com>
2024-06-27 00:48:14 -0700

    fixed route issues

--- src/App.js
+++ src/App.js
@@ -14,9 +14,9 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={About />} />
-        <Route path="/contact" element={Contact />} />
-        <Route path="/service" element={Services />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/service" element={<Services />} />
 
 
       </Routes>
